284f0ee775a04661f335d0fb601c0c3f9ae4a582
DannyelPina <nataniel06@live.com.pt>
2022-04-04 12:23:58 +0100

    add flag property to payment method DTO

--- src/repositories/IPaymentMethodsRepository.ts
+++ src/repositories/IPaymentMethodsRepository.ts
@@ -1,11 +1,12 @@
 interface IPaymentMethodDTO {
     id?: number;
     name: string;
+    flag?: string;
 }
 
 interface IPaymentMethodsRepository {
     getAll(): IPaymentMethodDTO[];
-    create(paymentMethod: IPaymentMethodDTO): void;
+    create(paymentMethod: IPaymentMethodDTO): IPaymentMethodDTO;
 }
 
 export { IPaymentMethodDTO, IPaymentMethodsRepository };
