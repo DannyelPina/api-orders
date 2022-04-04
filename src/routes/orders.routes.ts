ba513258188aa0a8dc95d2c93050fefece16db66
DannyelPina <nataniel06@live.com.pt>
2022-04-04 12:28:41 +0100

    add order routes

---
+++ src/routes/orders.routes.ts
@@ -0,0 +1,12 @@
+import { Router } from "express";
+import { createOrderController } from "../useCases/createOrder";
+
+const ordersRoutes = Router();
+
+ordersRoutes.post("/", (request, response) =>
+    createOrderController.handle(request, response)
+);
+
+ordersRoutes.get("/", (request, response) => {
+    response.send("Hello orders");
+});
