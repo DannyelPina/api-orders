c8693ef2b045c3f579216378a83b67792da2281d
DannyelPina <nataniel06@live.com.pt>
2022-04-04 12:26:54 +0100

    implement the findProductById and findPaymentMethodById fucntions

--- src/repositories/implementations/OrdersRepository.ts
+++ src/repositories/implementations/OrdersRepository.ts
@@ -1,6 +1,11 @@
-import { ORDERS } from "../../database";
+import { ORDERS, PAYMENTS_METHODS, PRODUCTS } from "../../database";
 import { Order } from "../../model/Order";
-import { IOrderDTO, IOrdersRepository } from "../IOrdersRepository";
+import {
+    IOrderDTO,
+    IOrdersRepository,
+    IProductOrderedDTO,
+} from "../IOrdersRepository";
+import { IPaymentMethodDTO } from "../IPaymentMethodsRepository";
 
 class OrdersRepository implements IOrdersRepository {
     private static INSTANCE: OrdersRepository;
@@ -21,6 +26,16 @@ class OrdersRepository implements IOrdersRepository {
         Object.assign(order, { customerId, paymentMethodId, products, amount });
 
         ORDERS.push(order);
+    }
+
+    findProductById(id: number): IProductOrderedDTO {
+        return PRODUCTS.find((product) => product.id === id);
+    }
+
+    findPaymentMethodById(id: number): IPaymentMethodDTO {
+        return PAYMENTS_METHODS.find(
+            (paymentMethod) => paymentMethod.id === id
+        );
     }
 }
 
